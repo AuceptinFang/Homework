f6585f616ee5406601be866d8eae96060239e066
AuceptinFang <wandererklein@icloud.com>
2025-02-26 13:15:25 +0800

    dist不在根目录

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/homework-system/', // 替换为您的GitHub仓库名称
+  base: '/Homework/',
 })
